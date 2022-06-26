a34ae476a815220d1fdd56972a461595b4984707
junseok oh <ixtears23@icloud.com>
2022-06-26 15:59:40 +0900

    password length, empty 메서드 분리 및 throw Exception 처리 변경으로 시그니처 void 변경

--- src/main/java/junseok/snr/study/business/password/PasswordStrengthVerifier.java
+++ src/main/java/junseok/snr/study/business/password/PasswordStrengthVerifier.java
@@ -2,8 +2,11 @@ package junseok.snr.study.business.password;
 
 public class PasswordStrengthVerifier {
 
-    public boolean verifyLength(String password) {
-        if (password == null) throw new PasswordLengthException("Password는 Null일 수 없습니다.");
-        return password.length() >= 8;
+    public void verifyBlank(String password) {
+        if (password == null || password.isEmpty()) throw new PasswordLengthException("Password 는 비어있을 수 없습니다.");
+    }
+
+    public void verifyLength(String password) {
+        if (password.length() < 8) throw new PasswordLengthException("패스워드는 8자리 이상이어야 합니다.");
     }
 }
