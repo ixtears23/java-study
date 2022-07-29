6302e70871aca5c2de07492f13c6e95e8ae8696c
junseok oh <ixtears23@icloud.com>
2022-07-30 00:07:29 +0900

    Multiple If Alternative RuleEngine - Multiply Test

--- src/test/java/junseok/snr/study/pattern/design/ruleengine/RuleEngineTest.java
+++ src/test/java/junseok/snr/study/pattern/design/ruleengine/RuleEngineTest.java
@@ -6,17 +6,24 @@ import junseok.snr.study.pattern.design.multipleif.ruleengine.Expression;
 import junseok.snr.study.pattern.design.multipleif.ruleengine.Operator;
 import junseok.snr.study.pattern.design.multipleif.ruleengine.Result;
 import junseok.snr.study.pattern.design.multipleif.ruleengine.RuleEngine;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RuleEngineTest {
 
+    private static RuleEngine ruleEngine;
+
+    @BeforeAll
+    static void setUp() {
+        ruleEngine = new RuleEngine();
+    }
+
     @Test
     @DisplayName("RuleEngine 덧셈 테스트")
     void addRuleTest() {
         Expression expression = new Expression(5, 5, Operator.ADD);
-        RuleEngine engine = new RuleEngine();
-        Result result = engine.process(expression);
+        Result result = ruleEngine.process(expression);
 
         assertThat(result).isNotNull();
         assertThat(result.getValue()).isEqualTo(10);
@@ -26,10 +33,18 @@ class RuleEngineTest {
     @DisplayName("RuleEngine 뺄셈 테스트")
     void subtractRuleTest() {
         final Expression expression = new Expression(10, 8, Operator.SUBTRACT);
-        final RuleEngine ruleEngine = new RuleEngine();
         final Result result = ruleEngine.process(expression);
 
         assertThat(result.getValue()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("RuleEngine 곱셈 테스트")
+    void multiplyRuleTest() {
+        final Expression expression = new Expression(10, 2, Operator.MULTIPLY);
+        final Result result = ruleEngine.process(expression);
+
+        assertThat(result.getValue()).isEqualTo(20);
     }
 
 }
