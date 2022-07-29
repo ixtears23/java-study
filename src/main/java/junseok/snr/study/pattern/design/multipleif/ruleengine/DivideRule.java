71f66c197c5d79cec989515b0ef69c10b1b48846
junseok oh <ixtears23@icloud.com>
2022-07-30 00:00:01 +0900

    Multiple If Alternative RuleEngine - DivideRule

---
+++ src/main/java/junseok/snr/study/pattern/design/multipleif/ruleengine/DivideRule.java
@@ -0,0 +1,21 @@
+package junseok.snr.study.pattern.design.multipleif.ruleengine;
+
+public class DivideRule implements Rule {
+
+    private int result;
+
+    @Override
+    public boolean evaluate(Expression expression) {
+        boolean evalResult = false;
+        if (expression.getOperator() == Operator.DIVIDE) {
+            evalResult = true;
+            result = expression.getY() / expression.getY();
+        }
+        return evalResult;
+    }
+
+    @Override
+    public Result getResult() {
+        return new Result(result);
+    }
+}
