d62b9a87f1c9070b6c0a9ff96b956eff61934ae4
junseok oh <ixtears23@icloud.com>
2022-07-30 00:04:11 +0900

    Multiple If Alternative RuleEngine - Subtract Test

--- src/test/java/junseok/snr/study/pattern/design/ruleengine/RuleEngineTest.java
+++ src/test/java/junseok/snr/study/pattern/design/ruleengine/RuleEngineTest.java
@@ -12,15 +12,24 @@ import org.junit.jupiter.api.Test;
 class RuleEngineTest {
 
     @Test
-    @DisplayName("Good")
-    void nothing() {
-        Expression expression = new Expression(5, 5, Operator.DIVIDE);
+    @DisplayName("RuleEngine 덧셈 테스트")
+    void addRuleTest() {
+        Expression expression = new Expression(5, 5, Operator.ADD);
         RuleEngine engine = new RuleEngine();
         Result result = engine.process(expression);
 
         assertThat(result).isNotNull();
         assertThat(result.getValue()).isEqualTo(10);
+    }
 
+    @Test
+    @DisplayName("RuleEngine 뺄셈 테스트")
+    void subtractRuleTest() {
+        final Expression expression = new Expression(10, 8, Operator.SUBTRACT);
+        final RuleEngine ruleEngine = new RuleEngine();
+        final Result result = ruleEngine.process(expression);
+
+        assertThat(result.getValue()).isEqualTo(2);
     }
 
 }
