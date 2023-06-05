8329b8773657e545cf42ac08d7509a112916bf19
ixtears23 <ixtears23@icloud.com>
2023-06-05 23:02:42 +0900

    [predicate] predicate 테스트 코드 리팩토링
    - @Test -> @ParameterizedTest
    - TestMethod DisplayName 내용 상세화
    - TestMethod DisplayName 중복 제거

--- src/test/java/junseok/snr/study/predicate/PredicateWithAndWithoutNameFilterTest.java
+++ src/test/java/junseok/snr/study/predicate/PredicateWithAndWithoutNameFilterTest.java
@@ -1,42 +1,47 @@
 package junseok.snr.study.predicate;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PredicateWithAndWithoutNameFilterTest {
+    private static final String DEFAULT_DISPLAY_NAME = "List [{1}]에서 문자열 [{0}]로 시작하는 것만 필터링했을 때 결과는 List [{2}]가 되어야 한다.";
+    private static PredicateWithAndWithoutNameFilter filter;
 
-    private PredicateWithAndWithoutNameFilter filter;
-    private List<String> names = new ArrayList<>();
-    private List<String> expectedNames = new ArrayList<>();
-
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         filter = new PredicateWithAndWithoutNameFilter();
-        names = Arrays.asList("Semi", "Hole", "Jane", "South", "Sonic");
-        expectedNames = Arrays.asList("Semi", "South", "Sonic");
     }
 
-    @DisplayName("Predicate를 사용하지 않고, List에서 파라미터로 받은 문자열로 시작하는 문자열 목록 필터링 테스트")
-    @Test
-    void filterWithoutPredicateTest() {
-        final List<String> filteredNames = filter.filterWithoutPredicate("S", names);
+    @MethodSource("provideDataForEqualityTest")
+    @ParameterizedTest(name = "Predicate를 사용하지 않고 " + DEFAULT_DISPLAY_NAME)
+    void filterWithoutPredicateTest(String start, List<String> names, List<String> expectedNames) {
+        final List<String> filteredNames = filter.filterWithoutPredicate(start, names);
 
         assertThat(filteredNames).isEqualTo(expectedNames);
     }
 
-    @DisplayName("Predicate를 사용하지 않고, List에서 파라미터로 받은 문자열로 시작하는 문자열 목록 필터링 테스트")
-    @Test
-    void filterWithPredicateTest() {
-        final List<String> filteredNames = filter.filterWithPredicate("S", names);
+    @MethodSource("provideDataForEqualityTest")
+    @ParameterizedTest(name = "Predicate를 사용해서 " + DEFAULT_DISPLAY_NAME)
+    void filterWithPredicateTest(String start, List<String> names, List<String> expectedNames) {
+        final List<String> filteredNames = filter.filterWithPredicate(start, names);
 
         assertThat(filteredNames).isEqualTo(expectedNames);
+    }
+
+    static Stream<Arguments> provideDataForEqualityTest() {
+        return Stream.of(
+                Arguments.of("S",
+                        Arrays.asList("Semi", "Hole", "Jane", "South", "Sonic"),
+                        Arrays.asList("Semi", "South", "Sonic"))
+        );
     }
 
 }
