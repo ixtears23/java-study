9bbaa2385f4b5564de82cbfeb196d71f7eac909b
ixtears23 <ixtears23@icloud.com>
2023-06-05 16:45:20 +0900

    [predicate] Integer 목록을 홀수(odd) List로 필터링하는 코드 추가

---
+++ src/main/java/junseok/snr/study/predicate/PredicateListOddFilter.java
@@ -0,0 +1,28 @@
+package junseok.snr.study.predicate;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class PredicateListOddFilter {
+
+    public static void main(String[] args) {
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Predicate<Integer> odd = number -> number % 2 != 0;
+        List<Integer> oddNumbers = filterNumbers(numbers, odd);
+
+        log.info(">>>>> numbers : {}", numbers);
+        log.info(">>>>> oddNumbers : {}", oddNumbers);
+    }
+
+    private static List<Integer> filterNumbers(List<Integer> numbers, Predicate<Integer> predicate) {
+        return numbers.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+}
