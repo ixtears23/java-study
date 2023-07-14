2ea518ed2dd1d9b3fa01564a13fa7b781be3f1dd
ixtears23 <ixtears23@icloud.com>
2023-07-14 23:50:10 +0900

    [feature] 3초작업 클래스 Task 구현 코드로 변경 #8

--- src/main/java/junseok/snr/study/thread/ThirdSecondsTask.java
+++ src/main/java/junseok/snr/study/thread/ThirdSecondsTask.java
@@ -3,21 +3,17 @@ package junseok.snr.study.thread;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ThirdSecondsTask {
+public class ThirdSecondsTask implements Task {
 
+    @Override
     public void execute() {
-        final long startTime = System.currentTimeMillis();
         int count = 0;
         for (int i = 0; i < 2_000_000_000; i++) {
             for (int j = 0; j < 1_000_000_000; j++) {
                 count++;
             }
         }
-
-        final long endTime = System.currentTimeMillis();
-        log.info(">>>>> loopCount : {}, execute time : {}", count, endTime - startTime);
     }
-
 
 
 }
