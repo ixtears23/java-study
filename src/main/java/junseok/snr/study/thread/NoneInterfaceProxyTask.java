9ef7b9e9d43db038a4e8340a1cb298f9a3aada8a
ixtears23 <ixtears23@icloud.com>
2023-07-15 01:07:19 +0900

    [feature] 인터페이스가 없는 프록시 클래스 추가 (#8)

---
+++ src/main/java/junseok/snr/study/thread/NoneInterfaceProxyTask.java
@@ -0,0 +1,19 @@
+package junseok.snr.study.thread;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NoneInterfaceProxyTask {
+    private final ThirdSecondsTask task;
+
+    public NoneInterfaceProxyTask(ThirdSecondsTask task) {
+        this.task = task;
+    }
+
+    public void execute() {
+        final long startTime = System.currentTimeMillis();
+        task.execute();
+        final long endTime = System.currentTimeMillis();
+        log.info(">>>>> execute time : {}", endTime - startTime);
+    }
+}
