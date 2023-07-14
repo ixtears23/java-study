05ad3bbf29052623b140aed1ff2ec1c0a8d40847
ixtears23 <ixtears23@icloud.com>
2023-07-14 23:53:41 +0900

    [feature] 프록시 클래스 구현 코드 추가 (#8)

--- src/main/java/junseok/snr/study/thread/ProxyTask.java
+++ src/main/java/junseok/snr/study/thread/ProxyTask.java
@@ -1,8 +1,20 @@
 package junseok.snr.study.thread;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ProxyTask implements Task {
+    private final Task task;
+
+    public ProxyTask(Task task) {
+        this.task = task;
+    }
+
     @Override
     public void execute() {
-
+        final long startTime = System.currentTimeMillis();
+        task.execute();
+        final long endTime = System.currentTimeMillis();
+        log.info(">>>>> execute time : {}", endTime - startTime);
     }
 }
