2cca15a5cb259a4fa65e5deaeea75cec2036f018
ixteasr23 <ixtears23@icloud.com>
2023-07-24 23:04:27 +0900

    [feauture] 단순 초 테스트

--- src/main/java/junseok/snr/study/Demo.java
+++ src/main/java/junseok/snr/study/Demo.java
@@ -1,4 +1,14 @@
 package junseok.snr.study;
 
+import java.util.Random;
+
 public class Demo {
+    public static void main(String[] args) {
+        int i = 0;
+        while ( i < 50 ) {
+            final int revenue = new Random().nextInt(3000) + 1000;
+            System.out.println(revenue);
+            i++;
+        }
+    }
 }
