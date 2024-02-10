8ac0b3a433f7a5fe381eb931c36b78bae060322d
JunseokOh <ixtears23@icloud.com>
2024-02-11 00:34:53 +0900

    [refactor] SpinLockTest 코드 개선

--- src/main/java/concurrent/SpinLockTest.java
+++ src/main/java/concurrent/SpinLockTest.java
@@ -6,24 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 public class SpinLockTest {
     private static final SpinLock spinLock = new SpinLock();
     public static void main(String[] args) {
-        Runnable task = () -> {
+        final Runnable task = () -> {
             log.info(Thread.currentThread().getName() + " LOCK 획득 시도");
             spinLock.lock();
             try {
                 log.info(Thread.currentThread().getName() + " LOCK 획득");
-                Thread.sleep(1000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             } finally {
                 spinLock.unlock();
-                log.info(Thread.currentThread().getName() + "LOCK 해제");
+                log.info(Thread.currentThread().getName() + " LOCK 해제");
             }
         };
 
-        Thread thread1 = new Thread(task);
-        Thread thread2 = new Thread(task);
+        final int loopCount = 100;
 
-        thread1.start();
-        thread2.start();
+        for (int i = 0; i < loopCount; i++) {
+            new Thread(task).start();
+        }
     }
 }
