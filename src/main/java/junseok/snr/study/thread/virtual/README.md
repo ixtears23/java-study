e03b130cb77d7bb0b91408945b1c7b581d6cb56c
JunseokOh <ixtears23@icloud.com>
2024-06-07 20:20:56 +0900

    [feature] Virtual Thread 문제 정의

---
+++ src/main/java/junseok/snr/study/thread/virtual/README.md
@@ -0,0 +1,3 @@
+Thread 내에서 10MB 배열을 할당하는 것과,
+Thread 실행 시 ThreadLocal 로 10MB의 배열을 가져오는 것의  
+HeapMemory Size와 ForkJoinPool의 Thread 생성 갯수가 왜 다른지 문제 풀기.
--- src/main/java/junseok/snr/study/thread/virtual/VirtualThreadPoolWithThreadSize.java
+++ src/main/java/junseok/snr/study/thread/virtual/VirtualThreadPoolWithThreadSize.java
@@ -6,9 +6,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class VirtualThreadPoolWithThreadLocal {
-//     10MB 할당
-    public static final ThreadLocal<byte[]> threadLocal = ThreadLocal.withInitial(() -> new byte[10 * 1024 * 1024]);
+public class VirtualThreadPoolWithThreadSize {
+    // 10MB 할당
+//    public static final ThreadLocal<byte[]> threadLocal = ThreadLocal.withInitial(() -> new byte[10 * 1024 * 1024]);
 
     @SneakyThrows
     public static void main(String[] args) {
@@ -18,8 +18,9 @@ public class VirtualThreadPoolWithThreadLocal {
         for (int i = 0; i < 100000; i++) {
             Thread.sleep(3);
             executorService.submit(() -> {
+                final int size = 10 * 1024 * 1024;
+                final byte[] bytes = new byte[size];
                 System.out.println(Thread.currentThread());
-                final byte[] bytes = threadLocal.get();
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
