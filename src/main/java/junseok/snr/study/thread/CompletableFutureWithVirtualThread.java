857028bf4dc9a0e851ee510edaae05d5fe14b75f
JunseokOh <ixtears23@icloud.com>
2024-06-07 19:26:49 +0900

    [feature] Create CompletableFutureWithVirtualThread

---
+++ src/main/java/junseok/snr/study/thread/CompletableFutureWithVirtualThread.java
@@ -0,0 +1,18 @@
+package junseok.snr.study.thread;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureWithVirtualThread {
+    public static void main(String[] args) {
+
+        final CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> "VirtualThread")
+                .thenApplyAsync(String::toUpperCase)
+                .thenAcceptAsync(upperCaseResult -> {
+                    System.out.println("Upper Case: " + upperCaseResult + " in Thread : " + Thread.currentThread().getName());
+                });
+
+        future.join();
+
+
+    }
+}
